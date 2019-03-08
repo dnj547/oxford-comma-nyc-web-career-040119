a20797c9e0df200fe6136384f3985f1e996826b8
Danielle Jasper <dnj547@live.com>
2019-03-08 18:49:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,7 @@
 def oxford_comma(array)
-  if array.size == 2
+  if array.size == 1
+    array
+  elsif array.size == 2
     array.join(' and ')
   else
     array[0..-2].join(', ').concat(", and #{array[-1]}")
